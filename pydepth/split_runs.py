f5e7b00fa2c8d087d4aa31c9e58cabe751f83ffd
Michael Millican <millican.hpc.isu@gmail.com>
2024-01-26 13:45:04 -0600

    updates

--- pydepth/split_runs.py
+++ pydepth/split_runs.py
@@ -3,6 +3,7 @@ from Bio import SeqIO
 import os
 import gzip
 import sys
+import glob
 
 
 def split_runs(input_file, split_path):
@@ -35,13 +36,24 @@ def split_runs(input_file, split_path):
             if run6 in record.id or record.description:
                 with gzip.open(f'{split_path}/run6/{filename}.run6.fastq.gz', 'wt') as out:
                     SeqIO.write(record, out, 'fastq')
+def get_input_files(input_file):
+    i_name = os.path.basename(input_file)
+    dir_path = os.path.dirname(input_file)
+    inl = i_name.split('_')
+    input_name = '_'.join(inl[:1])
+    f_file = glob.glob(f'{dir_path}/{input_name}*R1*')[0]
+    r_file = glob.glob(f'{dir_path}/{input_name}*R2*')[0]
+    return f_file, r_file, input_name
 
 def main():
-    input_name = sys.argv[1]
+    input_file = sys.argv[1]
     taxa = sys.argv[2]
-    f_file = f"/home/glbrc.org/millican/projects/scri/data/reads/{taxa}/{input_name}_R1_001.fastq.gz"
-    r_file = f"/home/glbrc.org/millican/projects/scri/data/reads/{taxa}/{input_name}_R2_001.fastq.gz"
+    f_file, r_file, input_name = get_input_files(input_file)
+
+    #f_file = f"/home/glbrc.org/millican/projects/scri/data/reads/{taxa}/{input_name}_R1_001.fastq.gz"
+    #r_file = f"/home/glbrc.org/millican/projects/scri/data/reads/{taxa}/{input_name}_R2_001.fastq.gz"
     split_path = f"/home/glbrc.org/millican/repos/microbiome_seq_depth/data/{taxa}/split_runs"
+    
     if os.path.exists(f_file) and os.path.exists(r_file):    
         # forward reads
         split_runs(f_file, split_path)
--- pydepth/split_runs.sh
+++ pydepth/split_runs.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-#source ~/.bashrc
+source ~/.bashrc
 #mamba activate trait-mapper
 #export PYTHONPATH=/home/glbrc.org/millican/mambaforge/envs/trait-mapper/lib/python3.1/site-packages:/home/glbrc.org/millican/mambaforge/envs/trait-mapper/lib/python3.11/site-packages
 #/home/glbrc.org/millican/repos/microbiome_seq_depth/pydepth/split_runs.py $1 $2
 
-process_16s_files()
+process_files()
 {
-    fname=$(basename -s "_R1_001.fastq.gz" "$1")
-    /home/glbrc.org/millican/repos/microbiome_seq_depth/pydepth/split_runs.py "$fname" "16S"
+    /home/glbrc.org/millican/repos/microbiome_seq_depth/pydepth/split_runs.py "$1" "$taxa"
+    if [ $? -ne 0 ]; then
+        echo "Error processing file $1" >&2
+        exit 1
+    fi
 }
 
-process_ITS_files()
-{
-    fname=$(basename -s "_R1_001.fastq.gz" "$1")
-    /home/glbrc.org/millican/repos/microbiome_seq_depth/pydepth/split_runs.py "$fname" "ITS"
-}
+export -f process_files
+export taxa=$1
 
-export -f process_16s_files
-export -f process_ITS_files
+find /home/glbrc.org/millican/projects/scri/data/reads/16S -maxdepth 1 -type f -name "*_R1_001.fastq.gz" | parallel -j 32 process_files
 
-find /home/glbrc.org/millican/projects/scri/data/reads/16S/ -name "*_R1_001.fastq.gz" | parallel -j 8 process_16s_files
-
-wait
-
-find /home/glbrc.org/millican/projects/scri/data/reads/ITS/ -name "*_R1_001.fastq.gz" | parallel -j 8 process_ITS_files
+if [ $? -ne 0 ]; then
+    echo "Error in parallel execution" >&2
+    exit 1
+fi
